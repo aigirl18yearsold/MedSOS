dedf18afa8305998c238ab9c195a5947c4e418d0
aigirl18yearsold <khatunshaila02@gmail.com>
2026-07-22 20:20:51 +0600

    Create lib/theme/app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1565C0);
+  static const secondary = Color(0xFF26A69A);
+
+  static const background = Color(0xFFF5F7FA);
+  static const surface = Colors.white;
+
+  static const success = Color(0xFF43A047);
+  static const warning = Color(0xFFFB8C00);
+  static const error = Color(0xFFE53935);
+
+  static const textPrimary = Color(0xFF1A1A1A);
+  static const textSecondary = Color(0xFF6B7280);
+}
