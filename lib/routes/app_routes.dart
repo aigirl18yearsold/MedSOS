8e2b8e3af1ff91a34c8027d2944cd38e90fdac57
aigirl18yearsold <khatunshaila02@gmail.com>
2026-07-23 10:14:30 +0600

    Add AppRoutes class with route constants

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,13 @@
+class AppRoutes {
+
+  static const splash = "/";
+
+  static const onboarding = "/onboarding";
+
+  static const login = "/login";
+
+  static const signup = "/signup";
+
+  static const home = "/home";
+
+}
