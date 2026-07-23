c28c58d4adaea8ce0728dc79daf0b06283393947
aigirl18yearsold <khatunshaila02@gmail.com>
2026-07-23 22:39:16 +0600

    Add OnboardingPage widget for user onboarding

---
+++ lib/features/auth/presentation/widgets/onboarding_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+
+  final IconData icon;
+  final String title;
+  final String description;
+
+  const OnboardingPage({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+
+      padding: const EdgeInsets.all(30),
+
+      child: Column(
+
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+
+          Icon(
+            icon,
+            size: 130,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+
+          const SizedBox(height: 40),
+
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          ),
+
+        ],
+
+      ),
+
+    );
+
+  }
+
+}
